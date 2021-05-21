5a87ff0f62c0cf6734b8a71ec3a0baba7bde1064
idunbar46 <75325755+idunbar46@users.noreply.github.com>
2021-05-21 11:52:57 +0100

    Add files via upload

--- idmain.tf
+++ idmain.tf
@@ -8,10 +8,25 @@ provider "azurerm" {
 #######################################################################
 
 resource "azurerm_resource_group" "id-test-rg" {
-  name     = "rgname"
+  name     = "id-test-rg"
   location = var.location
 
   tags = {
     environment = "my tag"
+  }
+}
+
+#######################################################################
+## Create Virtual Network
+#######################################################################
+resource "azurerm_virtual_network" "hub-vnet" {
+  name                = var.hubname
+  location            = var.location
+  resource_group_name = id-test-rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "hub-spoke"
+    deployment  = "terraform"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "location" {
   description = "Location to deploy resources"
   type        = string
 }
+
+variable "hubname" {
+  description = "Hub network name"
+  type        = string
+}
